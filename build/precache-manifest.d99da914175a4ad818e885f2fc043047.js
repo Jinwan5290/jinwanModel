7633f0b8bda9155975898c5503a648552bbc1750
jinwanyang <jinwanyang@tencent.com>
2021-02-20 09:58:57 +0800

    fix test1

---
+++ build/precache-manifest.d99da914175a4ad818e885f2fc043047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e805eb90cf9264cb286c096d84c89e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770cbf2a171fda166efe",
+    "url": "/static/css/main.1aaf6168.chunk.css"
+  },
+  {
+    "revision": "4a94ab6c987b79bd02b3",
+    "url": "/static/js/2.0a06afcf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0a06afcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770cbf2a171fda166efe",
+    "url": "/static/js/main.0499563d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import './index.css'
 class App extends React.Component<any, any> {
   render() {
-    return <div>hello jinwan</div>
+    return (
+      <div className="flexbox">
+        <div className="flexItem">1</div>
+        <div className="flexItem">222</div>
+        <div className="flexItem">3333</div>
+        <div className="flexItem">4</div>
+        <div className="flexItem">5</div>
+        <div className="flexItem">6</div>
+        <div className="flexItem">7</div>
+        <div className="flexItem">8</div>
+      </div>
+    )
   }
 }
 
